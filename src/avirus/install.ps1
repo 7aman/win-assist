035d4c3d0184f41b9e09e86b7e9b38fd9f8cc515
7aman <7amanama7@gmail.com>
2018-10-27 13:07:31 +0330

    refactoring avirus install. add .ico file

--- src/avirus/install.ps1
+++ src/avirus/install.ps1
@@ -2,14 +2,14 @@ $Name = "avirus"
 $scriptPath = $PSScriptRoot
 # bin Folder
 $shrtDst = Split-Path (Split-Path $scriptPath -Parent) -Parent 
-
+$PWERSHELL = "C:\Windows\System32\WindowsPowershell\v1.0\powershell.exe"
 $WshShell = New-Object -comObject WScript.Shell
 
 $shrt = $shrtDst + "\$Name.lnk"
 $MyShortcut = $WshShell.CreateShortcut($shrt)
 $MyShortcut.WindowStyle = 3
 $MyShortcut.IconLocation = $scriptPath +"\$Name.ico"
-$MyShortcut.TargetPath = "C:\Windows\System32\WindowsPowershell\v1.0\powershell.exe"
+$MyShortcut.TargetPath = $PWERSHELL
 $MyShortcut.Arguments =  "$scriptPath\$Name.ps1"
 $MyShortcut.Save()
 
