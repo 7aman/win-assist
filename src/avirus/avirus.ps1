3e1fc0a0bb7ff45560ed16b8cc2e757123c0f3a1
7aman <7amanama7@gmail.com>
2018-10-27 12:31:00 +0330

    new approach

---
+++ src/avirus/avirus.ps1
@@ -0,0 +1,28 @@
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")){ 
+	Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit 
+}
+function print_help {
+    Write-Host  "Usage:"  -ForegroundColor Green
+    Write-Host  "   avirus on"  -ForegroundColor Green
+    Write-Host  "               : to enable antivirus"  -ForegroundColor Green
+    Write-Host  "   avirus off"  -ForegroundColor Green
+    Write-Host  "               : to disable antivirus ( no argument or anything that is not 'on'"  -ForegroundColor Green
+    Write-Host "*********************************************************************************"
+}
+Switch ($args[0]) {
+    "on" {
+        Write-Host  "Enabling 'Real-time Protection' for Michrosoft Windows Defender Anti-Virus..."  -ForegroundColor Yellow
+        Set-MpPreference -DisableRealtimeMonitoring $false | Wait-Job
+        Write-Host  "Done!"  -ForegroundColor Green
+    }
+    "off"   {
+        Write-Host  "Disabling 'Real-time Protection' for Michrosoft Windows Defender Anti-Virus..."  -ForegroundColor Yellow
+        Set-MpPreference -DisableRealtimeMonitoring $true | Wait-Job
+        Write-Host  "Done!"  -ForegroundColor Green
+     }
+    default {
+        print_help
+    }
+}
+Pause
+
--- src/setup/Install.ps1
+++ src/setup/Install.ps1
@@ -7,35 +7,37 @@ function Get-User-Only-Paths {
 function Add-To-Path {
     if ($Root -In $user_only.Split(";") ) {
         Write-Host $Root," is already in User Path."
-        $Setting.Settings.Path.addedByMe = $false
-        $Setting.Settings.Path.path = "{$Root}"
+        # $Setting.Settings.Path.addedByMe = $false
+        # $Setting.Settings.Path.path = "{$Root}"
         return $false
     } else {
         [Environment]::SetEnvironmentVariable("Path", $Root + ";" + $user_only , "User")
         Write-Host $Root," is added to User Path."
-        $Setting.Settings.Path.addedByMe = $false
-        $Setting.Settings.Path.path = "{$Root}"
+        # $Setting.Settings.Path.addedByMe = $false
+        # $Setting.Settings.Path.path = "{$Root}"
         return $true
     }
 }
 
-function Write-Path-To-File {
-    $file = ($PSScriptRoot) + "\path.txt"
-    Out-File -FilePath $file -InputObject $Root -Encoding utf8 
-}
-[XML]$Setting = Get-Content -Path ".\setup\setting.xml"
-Write-Host $Setting.Settings.Install.Installed
-Write-Host $Setting.Settings.Path.addedByMe
-Write-Host $Setting.Settings.Path.path
+# function Write-Path-To-File {
+#     $file = ($PSScriptRoot) + "\path.txt"
+#     Out-File -FilePath $file -InputObject $Root -Encoding utf8 
+# }
+# [XML]$Setting = Get-Content -Path ".\setup\setting.xml"
+# Write-Host $Setting.Settings.Install.Installed
+# Write-Host $Setting.Settings.Path.addedByMe
+# Write-Host $Setting.Settings.Path.path
 $Root = Split-Path (Split-Path $PSScriptRoot -Parent) -Parent
 $user_only = $(Get-User-Only-Paths)
-if ($Setting.Settings.Install.Installed){
-    Write-Host "Already Installed. Use force mode?"
-} else {
-    Add-To-Path
-    $Setting.Settings.Install.Installed = $false
-}
-$Setting.Save(".\setup\setting.xml")
+Add-To-Path
+
+# if ($Setting.Settings.Install.Installed){
+#     Write-Host "Already Installed. Use force mode?"
+# } else {
+    # Add-To-Path
+    # $Setting.Settings.Install.Installed = $false
+# }
+# $Setting.Save(".\setup\setting.xml")
 
 # Write-Path-To-File
 # foreach ($path in ([System.Environment]::GetEnvironmentVariable("Path", "User")).split(";")){
@@ -47,3 +49,15 @@ $Setting.Save(".\setup\setting.xml")
 #     Write-Host $path
 # }
 # Pause
+
+# <?xml version="1.0" encoding="utf-8"?>
+#   <Settings>
+#     <Path>
+#       <path>C:\bin</path>
+#       <addedByMe>true</addedByMe>
+#     </Path>
+#     <Install>
+#       <Installed>true</Installed>
+#     </Install>
+#   </Settings>
+
