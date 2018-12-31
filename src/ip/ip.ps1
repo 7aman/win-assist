8423f4ee30b3e0cd887b9c5814abbc6de37f6d71
7aman <7amaaan@gmail.com>
2018-12-31 11:00:12 +0330

    IP Set, IP List completed

--- src/ip/ip.ps1
+++ src/ip/ip.ps1
@@ -1,7 +1,7 @@
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")){ 
 	Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit 
 }
-$SUBNETS = @(
+$global:SUBNETS = @(
     "255.255.255.0",
     "128.0.0.0",
     "192.0.0.0",
@@ -36,157 +36,116 @@ $SUBNETS = @(
     "255.255.255.254",
     "255.255.255.255"
 )
-$global:Arguments = $args
-# Default Values
-# $global:PublicNIC = "Wi-Fi"
-# $global:PrivateNIC = "Ethernet"
-# $global:PrivateIP = "192.168.1.199"
-# $global:PrivateGateway = "192.168.1.2"
-# $global:PrivateSubnet = 0
 
+$global:Arguments = $args
 
 function Write-AdapterNames {
     Write-Host "Available Net Adapters are:" -ForegroundColor Green
     $NIC_list = Get-NetAdapter | Select-Object -Property Name
-    foreach ($NIC in $NIC_list) {
-        Write-Host "    "$NIC.Name -ForegroundColor Cyan
-    }
+    foreach ($NIC in $NIC_list) { Write-Host "    "$NIC.Name -ForegroundColor Cyan }
     Pause
     Exit
 }
 function Test-ActionArgument {
     $ACCEPTED = @("list", "set", "del", "add", "share")
     $action = $global:Arguments[0]
-    if ($ACCEPTED -contains $action){
-        return $action
-    }
+    if ($ACCEPTED -contains $action) { return $action }
     else {
         Write-Host "Invalid action argument."
         Write-Host "Valid action arguments are:"
-        foreach ($action in $ACCEPTED) {
-            Write-Host "    "$action -ForegroundColor Cyan
-        } 
+        foreach ($action in $ACCEPTED) { Write-Host "    "$action -ForegroundColor Cyan } 
         Pause
         Exit
     }
 }
 
 
-# Argument Validation
-
 function Test-AdapterName($given_string){
-    if( $(Get-NetAdapter -Name $given_string -ErrorAction SilentlyContinue -InformationAction SilentlyContinue)){
-        return $true
-    }
-    return $false
+    if( $(Get-NetAdapter -Name $given_string -ErrorAction SilentlyContinue -InformationAction SilentlyContinue)) { return $true }
+    else { return $false }
 }
 function Test-IPSubnet($given_string){
     $ValidIpAddressRegex = [regex] "^((?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?))$"
     $ValidSubnetRegex = [regex]"^([1-9]|[1-2][0-9]?|3[0-2])$"
     $ValidIPSubnetRegex = [regex] "^((?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?))/([1-9]|[1-2][0-9]?|3[0-2])$"
-
-    if ($given_string -match $ValidIpAddressRegex){
-        return @($given_string, $false)
-    }
-    elseif ($given_string -match $ValidSubnetRegex) {
-        return @($false, $given_string)
-        
-    }
+    if ($given_string -match $ValidIpAddressRegex) { return @($given_string, $false) }
+    elseif ($given_string -match $ValidSubnetRegex) { return @($false, $given_string) }
     elseif ($given_string -match $ValidIPSubnetRegex) {
         $IPSUBNET = $ValidIPSubnetRegex.Match($given_string).groups
         return @($IPSUBNET[1].Value, $IPSUBNET[2].Value)
-    } else {
-        return @($false, $false)
     }
+    else { return @($false, $false) }
 }
-function  List-IP {
-    $arg = $global:Arguments[1]
-
-    if ($arg -eq "nics"){
-        Write-AdapterNames
-    }elseif (Test-AdapterName($arg)){
-        Start-Process  netsh "interface ip show addresses",$arg -NoNewWindow -wait
-    }else {
-        Write-Host "    '$arg' is not a valid NIC." -ForegroundColor Red
-        Write-AdapterNames
+function  Show-IPList {
+    $NIC = ""
+    if ($global:Arguments[1]) {
+        $arg = $global:Arguments[1]
+        if ($arg -eq "nics") { Write-AdapterNames }
+        elseif (Test-AdapterName($arg)) { $NIC = $arg }
+        else {
+            Write-Host "    given argument($arg) is not a valid NIC." -ForegroundColor Red
+            Write-Host "    using 'Ethernet' as default NIC." -ForegroundColor Red
+            Write-Host "    you can see list of available NICs by 'ip list nics'." -ForegroundColor Red
+            $NIC = "Ethernet"
+        }
     }
+    else {
+        Write-Host "    no arguments is presented." -ForegroundColor Yellow
+        Write-Host "    using 'Ethernet' as default NIC." -ForegroundColor Yellow
+        Write-Host "    you can see list of available NICs by 'ip list nics'." -ForegroundColor Yellow
+        $NIC = "Ethernet"
+    }
+    Start-Process  netsh "interface ip show config",$NIC -NoNewWindow -wait
 }
 
 function  Set-IP {
-    # Write-Host "Argument Checking:" -ForegroundColor Yellow
     $ARGSS = $global:Arguments | Select-Object -Skip 1
     $NIC = ""
     $IPS = @()
     $SUB = ""
     foreach ($arg in $ARGSS){
-        if (Test-AdapterName($arg)){
-            $NIC = $arg
-        }
+        if (Test-AdapterName($arg)) { if (!$NIC) { $NIC = $arg } }
         $IP, $eSUB = Test-IPSubnet($arg)
-        if ($eSub){
-            $SUB = $eSUB
-        }
-        if ($IP){
-            $IPS += $IP
-        }
+        if ($eSub) { if (!$SUB) { $SUB = $eSUB } }
+        if ($IP) { $IPS += $IP }
     }
-    Write-Host  "Command to run:" -ForegroundColor Yellow
-    Write-Host  "netsh interface ip set address "$NIC"  static  "$IP" "$SUBNET $GATEWAY -ForegroundColor Cyan
-    Write-Host ""
-    Write-Host "Result: "
-
-    netsh interface ip set address $NIC  static $IP $SUBNET $GATEWAY
-
-    if (!$LastExitCode){
+    if (!$SUB) { $SUB = 24 }
+    if (!$NIC) { $NIC = "Ethernet" }
+    if (!$IPS) {
+        $IPS += "192.168.1.199"
+        $IPS += ""
+    }
+    Write-Host "Setting IP Address using this command:" -ForegroundColor Green
+    Write-Host "    netsh interface ip set address $NIC  static "$IPS[0], $global:SUBNETS[$SUB],$IPS[1]
+    Start-Process netsh "interface ip set address",$NIC,"static",$IPS[0],$global:SUBNETS[$SUB],$IPS[1] -NoNewWindow -wait
+    if (!$LastExitCode) {
         Write-Host "OK. IP is set successfully." -ForegroundColor  Green
+        Write-Host
     }
-    Write-Host "Getting"$NIC" interface information..." -ForegroundColor  Yellow
-    Start-Sleep 4
-    Start-Process  netsh "interface ip show addresses",$NIC -NoNewWindow -wait
-}
-function  Test-OtherArguments {
-    # Write-Host "Argument Checking:" -ForegroundColor Yellow
-    $ARGSS = $global:Arguments | Select-Object -Skip 1
-    $counter = 0
-    foreach ($arg in $ARGSS){
-
-        if ($arg -eq "nics"){
-            Write-AdapterNames
+    if ($IPS[2]) {
+        Write-Host "Setting Primary DNS using this command:" -ForegroundColor Green
+        Write-Host "    netsh interface ip set dnsservers $NIC static",$IPS[2]
+        Start-Process netsh "interface ip set dnsserver",$NIC,"static",$IPS[2] -NoNewWindow -wait
+        if (!$LastExitCode) {
+            Write-Host "OK. Primary DNS is set successfully." -ForegroundColor  Green
+            Write-Host
         }
-
-        if (Test-AdapterName($arg)){
-            if ($counter -eq 0){
-                $global:PrivateNIC = $arg
-                Write-Host "    '$arg' will be used as private NIC."  -ForegroundColor Green
-                $counter++
-                continue
-            } elseif ($counter -eq 1) {
-                $global:PublicNIC = $arg
-                Write-Host "    '$arg' will be used as public NIC." -ForegroundColor Green
-                $counter++
-                continue
-            } else {
-                Write-Host "More than two NIC is given." -ForegroundColor Red
-                Pause
-                Exit
+        if ($IPS[3]) {
+            Write-Host "Setting Alternative DNS using this command:" -ForegroundColor Green
+            Write-Host "    netsh interface ip add dnsservers $NIC",$IPS[3],"index=2"
+            Start-Process netsh "interface ip add dnsserver",$NIC,$IPS[3],"index=2" -NoNewWindow -wait
+            if (!$LastExitCode) {
+                Write-Host "OK. Alternative DNS is set successfully." -ForegroundColor  Green
+                Write-Host
             }
         }
-        # check for ip or subnet
-        elseif (Test-IPSubnet($arg)){
-                continue
-        }
-        else {
-            Write-Host "    '$arg' is not valid. Ignored." -ForegroundColor Red
-        }
-    }
-}
-$action = $(Test-ActionArgument)
-if ($action -eq "list"){
-    List-IP
-} elseif ($action -eq "set"){
-    Set-IP
+    }  
 
 }
+
+$action = $(Test-ActionArgument)
+if ($action -eq "list") { Show-IPList }
+elseif ($action -eq "set") { Set-IP }
 
 Write-Host "Done!" -ForegroundColor Green
 Pause
