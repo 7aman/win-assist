bf6793e2407aa89537551c39a5a7ac24c7adeb2b
7aman <7amaaan@gmail.com>
2019-06-27 11:20:47 +0430

    bugfix

--- src/uart/uart.ps1
+++ src/uart/uart.ps1
@@ -8,13 +8,13 @@ param(
 )
 <#
     Any single digit from 5 to 9 sets the number of data bits.
-    ‘1’, ‘1.5’ or ‘2’ sets the number of stop bits.
+    '1', '1.5' or '2' sets the number of stop bits.
     Any other numeric string is interpreted as a baud rate.
-    A single lower-case letter specifies the parity: ‘n’ for none, ‘o’ for odd, ‘e’ for even, ‘m’ for mark and ‘s’ for space.
-    A single upper-case letter specifies the flow control: ‘N’ for none, ‘X’ for XON/XOFF, ‘R’ for RTS/CTS and ‘D’ for DSR/DTR.
-For example, ‘-sercfg 19200,8,n,1,N’ denotes a baud rate of 19200, 8 data bits, no parity, 1 stop bit and no flow control. 
+    A single lower-case letter specifies the parity: 'n' for none, 'o' for odd, 'e' for even, 'm' for mark and 's' for space.
+    A single upper-case letter specifies the flow control: 'N' for none, 'X' for XON/XOFF, 'R' for RTS/CTS and 'D' for DSR/DTR.
+For example, '-sercfg 19200,8,n,1,N' denotes a baud rate of 19200, 8 data bits, no parity, 1 stop bit and no flow control. 
 #>
-
+Push-Location -Path "$PSScriptRoot"
 Write-Host "Closing existing PUTTY sessions"
 $old_putty = Get-Process -Name "PUTTY" -WarningAction SilentlyContinue -ErrorAction SilentlyContinue
 if ($old_putty){
