a0d199800a112ce70c2098ef50308c216a759fd3
Zaman <7amaaan@gmail.com>
2019-09-03 17:17:19 +0430

    add plink

--- src/uart/uart.ps1
+++ src/uart/uart.ps1
@@ -1,10 +1,11 @@
 param(
-	$port = "COM3",
-	$baud_rate = 115200,
-	$data_bits = 8,
-	$stop_bits = 1,
-	$parity = 'n',
-	$flow_control = 'X'
+    $app = "putty",
+    $port = "COM3",
+    $baud_rate = 115200,
+    $data_bits = 8,
+    $stop_bits = 1,
+    $parity = 'n',
+    $flow_control = 'X'
 )
 <#
     Any single digit from 5 to 9 sets the number of data bits.
@@ -14,11 +15,47 @@ param(
     A single upper-case letter specifies the flow control: 'N' for none, 'X' for XON/XOFF, 'R' for RTS/CTS and 'D' for DSR/DTR.
 For example, '-sercfg 19200,8,n,1,N' denotes a baud rate of 19200, 8 data bits, no parity, 1 stop bit and no flow control. 
 #>
-Push-Location -Path "$PSScriptRoot"
-Write-Host "Closing existing PUTTY sessions"
-$old_putty = Get-Process -Name "PUTTY" -WarningAction SilentlyContinue -ErrorAction SilentlyContinue
-if ($old_putty){
-	 Stop-Process  $old_putty
+
+function Print-Help {
+	Write-Host "TODO"
 }
-$putty_args = "-serial $port -sercfg $baud_rate,$data_bits,$parity,$stop_bits,$flow_control"
-start-process .\putty\putty.exe $putty_args
+function Stop-OldProcess {
+	$procs = @("PUTTY", "plink")
+	foreach ($proc in $procs) {
+		$old_process = Get-Process -Name $proc -WarningAction SilentlyContinue -ErrorAction SilentlyContinue
+		if ($old_process) {
+			Write-Host "Closing existing $proc sessions"
+			Stop-Process  $old_process
+		}
+	}
+}
+
+function Start-Putty ($arguments) {
+	Start-Process .\putty\putty.exe $arguments
+}
+
+function Start-PLink ($arguments) {
+	$logfile = ".\logs\uart-" + $(get-date -f yyyyMMdd-HHmmss) + ".log"
+	New-Item -ItemType Directory -Force -Path $(Split-Path $logfile -Parent) | Out-Null
+    Start-Process "powershell.exe" -ArgumentList ".\plink\plink.exe",$arguments," | Tee-Object $logfile" 
+}
+
+Push-Location -Path "$PSScriptRoot"
+Stop-OldProcess
+$arguments = "-v -serial $port -sercfg $baud_rate,$data_bits,$parity,$stop_bits,$flow_control"
+
+if ($app -eq "putty") { 
+	Start-Putty $arguments
+}
+elseif ($app -eq "plink") {
+	Start-PLink $arguments
+}
+else {
+	Print-Help
+}
+
+Exit
+
+
+
+
